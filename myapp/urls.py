628a67078a199b628f120d472bd78d7282aa67d1
Anand040593 <134695730+Anand040593@users.noreply.github.com>
2025-06-27 19:33:35 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -34,7 +34,6 @@ urlpatterns = [
     # path('submit-candidate/', submit_candidate, name='submit_candidate'),#TODO screen to develop and integrated
     path('api/match-resumes/', ResumeMatchingAPI.as_view(), name='match_resumes'),#TODO screen to develop and integrated
     path('api/', include(router.urls)), #TODO Hiring Manager create requsition
-    path('api/', include(router1.urls)), #TODO home Screen Hiring Manager
     path("api/upload-resumes/", BulkUploadResumeView.as_view(), name="bulk-upload-resumes"), #TODO screen to develop and integrated
     path("forgot-password/", ForgotPasswordView.as_view(), name="forgot_password"),#TODO screen to develop and integrated
     path("reset-password/", ResetPasswordView.as_view(), name="reset_password"),#TODO screen to develop and integrated
@@ -46,12 +45,27 @@ urlpatterns = [
     path('interview_planner_calc/', InterviewPlannerCalculation.as_view(), name='interview_planner_calc'),#TODO screen to develop and integrated
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('interview_design_screen/', InterviewDesignScreenView.as_view(), name='interview_design_screen'),
+    path('state_alert_responsibility/', StateAlertResposibilityView.as_view(), name='state_alert_responsibility'),
+    # path('interviewer_calender/', InterviewerCalenderView.as_view(), name='interviewer_calender'),
+    path('candidate_interview_stages/', CandidateInterviewStagesView.as_view(), name='candidate_interview_stages'),
 
-    path('api/hiring-plans/', get_hiring_plans, name='hiring_plans'),
-    path('api/hiring-plan/details/', get_hiring_plan_details, name='hiring_plan_details'),
-    path('get_requisition_by_id/', JobRequisitionViewSet.as_view({'post': 'get_requisition_by_id'}), name='get_requisition_by_id'),
+    path('api/hiring-plans/', get_hiring_plans, name='hiring_plans'), #TODO screen to develop and integrated
+    path('api/hiring-plan/details/', get_hiring_plan_details, name='hiring_plan_details'), #TODO screen to develop and integrated
+    path('get_requisition_by_id/', JobRequisitionViewSet.as_view({'post': 'get_requisition_by_id'}), name='get_requisition_by_id'), #TODO screen to develop and integrated
 
-    path('api/', include(router2.urls)),
+    path('api/', include(router1.urls)),#TODO home screen to develop and integrated
+    path('api/', include(router2.urls)),#TODO get template to develop and integrated
+    path("candidates/screening/", CandidateScreeningView.as_view(), name="candidate-screening"),#TODO screen to develop and integrated
+    path("candidates/schedule-meet/", ScheduleMeetView.as_view(), name="schedule-meet"),# TODO testing in process
+    
+    path('api/schedule/context/', ScheduleContextAPIView.as_view()),
+    path('api/interviewers/', InterviewerListCreateAPIView.as_view(), name='interviewer-list-create'),# create interviewer slot
+
+    path('api/interview/review/', SubmitInterviewReviewView.as_view()),  # Submitting feedback
+    path('api/interview/report/', InterviewReportAPIView.as_view()),  # Generating interview report
+
+    path("api/interview/schedules/", GetInterviewScheduleAPIView.as_view()),
 
 ]
 
